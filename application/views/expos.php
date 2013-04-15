a2cbabbcf550766279f08e14b88b4f9134997780
vishnu.kanduri <vishnu.kanduri@maxsoftware.com.au>
2013-04-15 10:44:19 +1000

    changed expo from view to grid layout

--- application/views/expos.php
+++ application/views/expos.php
@@ -1,36 +1,89 @@
 <? $this->load->view('include/header');?>
 <? $this->load->view('include/admin_navbar');?>
 <div class="wrapper">
-    <div class="row">
-     <div class="span12">
-     <form class="form-horizontal myform"action="#"method="post">
-    <fieldset>
-      <h3>View an Expo</h3>
-      <div class="control-group">
-       <label class="control-label"for="expoDate">Expo Date</label>
-        <div class="controls">
-         <div class="input-prepend">
-          <span class="add-on">
-           <i class="icon-calendar"></i></span>
-           <input type="text"class="input-medium"placeholder="9/04/2013" readonly="true">
-         </div>
-       </div>
+  <div class="row">
+    <div class="span12">
+     <div class="side_button">
+       <p><a href="edit_expo.html"class="btn btn-primary">Create an Expo</a></p>
      </div>
-     <div class="control-group">
-      <label class="control-label"for="exponame">Expo Name</label>
-      <div class="controls">
-        <input type="text"class="input-medium" name="expoName" placeholder="Test Expo" readonly="true">
-      </div>
-    </div>
-    <div class="control-group">
-      <label class="control-label"for="location">Location</label>
-      <div class="controls">
-        <input type="text"class="input-medium"name="location"placeholder="Melbourne" readonly="true">
-      </div>
-    </div>
-     </fieldset>
-      </form>
-    </div>
-  </div>
+     <h3>Expos</h3>
+     <table class="table table-bordered table-striped">
+      <tr>
+        <th>Expo Date</th>
+        <th>Expo Name</th>
+        <th>Location</th>
+        <th>Actions</th>
+      </tr>
+      <tr>
+        <td>8/04/2013</td>
+        <td>DJ Expo Meet</td>
+        <td>Melbourne</td>
+        <td><a class="btn btn-success" href="view_expo.html">
+          <i class="icon-zoom-in icon-white"></i>    View                                            
+        </a>
+        <a class="btn btn-info" href="edit_expo.html">
+          <i class="icon-edit icon-white"></i>  
+          Edit                                            
+        </a>
+        <a class="btn btn-danger" href="#">
+          <i class="icon-trash icon-white"></i> 
+          Delete
+        </a>
+      </td>
+    </tr>
+      <tr>
+        <td>18/04/2013</td>
+        <td>DJ Expo Meet</td>
+        <td>Melbourne</td>
+        <td><a class="btn btn-success" href="view_expo.html">
+          <i class="icon-zoom-in icon-white"></i>    View                                            
+        </a>
+        <a class="btn btn-info" href="edit_expo.html">
+          <i class="icon-edit icon-white"></i>  
+          Edit                                            
+        </a>
+        <a class="btn btn-danger" href="#">
+          <i class="icon-trash icon-white"></i> 
+          Delete
+        </a>
+      </td>
+    </tr>
+      <tr>
+        <td>29/04/2013</td>
+        <td>DJ Expo Meet</td>
+        <td>Sydney</td>
+        <td><a class="btn btn-success" href="view_expo.html">
+          <i class="icon-zoom-in icon-white"></i>    View                                            
+        </a>
+        <a class="btn btn-info" href="edit_expo.html">
+          <i class="icon-edit icon-white"></i>  
+          Edit                                            
+        </a>
+        <a class="btn btn-danger" href="#">
+          <i class="icon-trash icon-white"></i> 
+          Delete
+        </a>
+      </td>
+    </tr>
+      <tr>
+        <td>30/04/2013</td>
+        <td>DJ Expo Meet</td>
+        <td>Perth</td>
+        <td><a class="btn btn-success" href="view_expo.html">
+          <i class="icon-zoom-in icon-white"></i>    View                                            
+        </a>
+        <a class="btn btn-info" href="edit_expo.html">
+          <i class="icon-edit icon-white"></i>  
+          Edit                                            
+        </a>
+        <a class="btn btn-danger" href="#">
+          <i class="icon-trash icon-white"></i> 
+          Delete
+        </a>
+      </td>
+    </tr>
+  </table>
+</div>
+</div><!--/end of span12-->
 </div><!--/end of wrapper-->
 <? $this->load->view('include/footer');?>
